a9d71501f24e33b3091a38911c43921a385f5743
FacuGaymer123 <90059465+FacuGaymer123@users.noreply.github.com>
2022-11-11 12:01:09 -0300

    xd

--- resources/views/client/test.blade.php
+++ resources/views/client/test.blade.php
@@ -1,70 +1,61 @@
-@extends('layouts.client')
+@include('partials.menu_head')
+@include('partials.navbar')
 
-@section('content')
-<style>
-    .jose{
-        margin: 0 auto;
-    }
-    .radio{
-        display: inline-flex;
-    }
-</style>
-    <div class="container">
+
+<div class="container">
+    <div>
         <div>
-            <div>
-                <div style="background-color: #024d5e; " class="card">
+            <div class="mt-5" style="background-color: #024d5e; " class="card">
 
+                <div>
+                    @if (session('status'))
                     <div>
-                        @if (session('status'))
-                            <div>
-                                <div>
-                                    <div class="alert alert-success" role="alert">
-                                        {{ session('status') }}
-                                    </div>
-                                </div>
+                        <div>
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
                             </div>
-                        @endif
-
-                        <form method="POST" action="{{ route('client.test.store') }}">
-                            @csrf
-                            @foreach ($categories as $category)
-                                @foreach ($category->categoryQuestions as $question)
-                                    <div style="background-color: #17a2b4; color: white; ">
-                                        <div>{{ $question->question_text }}</div>
-
-                                        <div style="background-color: #707070; color: white; ">
-                                            <input type="hidden" name="questions[{{ $question->id }}]" value="">
-                                            @foreach ($question->questionOptions as $option)
-                                                <div class="content-input">
-                                                    <label for="myRadioID" class="radio">
-                                                    <input type="radio" name="myRadioField" id="myRadioID" class="radio__input" name="questions[{{ $question->id }}]">
-                                                        <div>
-                                                        id="option-{{ $option->id }}"
-                                                        value="{{ $option->id }}"@if (old("questions.$question->id") == $option->id) checked @endif>{{ $option->option_text }}
-                                                        </div class="radio__radio">
-                                                    </label>
-
-                                                    </div>
-                                            @endforeach
-                                        </div>
-                                        @if ($errors->has("questions.$question->id"))
-                                            <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;"
-                                                role="alert">
-                                                <strong>{{ $errors->first("questions.$question->id") }}</strong>
-                                            </span>
-                                        @endif
-
-                                    </div>
-                                @endforeach
-                            @endforeach
-                            <input class="jose" type="submit">
+                        </div>
                     </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('client.test.store') }}">
+                        @csrf
+                        @foreach ($categories as $category)
+                        @foreach ($category->categoryQuestions as $question)
+                        <div style="background-color: #17a2b4; color: white; ">
+                            <div>{{ $question->question_text }}</div>
+                            <img src="{{ $question-> img }}" alt="">
+                            <div style="background-color: #707070; color: white; ">
+                                <input type="hidden" name="questions[{{ $question->id }}]" value="">
+                                @foreach ($question->questionOptions as $option)
+                                <div class="content-input">
+                                    <label for="myRadioID">
+                                        <input type="radio" name="questions[{{ $question->id}}]"
+                                            id="option-{{ $option->id }}" class="radio__input" value="{{ $option->id }}"
+                                            @if (old("questions.$question->id") == $option->id) checked @endif>{{
+                                        $option->option_text }}
+
+                                    </label>
+
+                                </div>
+                                @endforeach
+                            </div>
+                            @if ($errors->has("questions.$question->id"))
+                            <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;" role="alert">
+                                <strong>{{ $errors->first("questions.$question->id") }}</strong>
+                            </span>
+                            @endif
+
+                        </div>
+                        @endforeach
+                        @endforeach
+                        <input type="submit">
                 </div>
             </div>
-            </form>
         </div>
+        </form>
     </div>
-    </div>
-    </div>
-    </div>
-@endsection
+</div>
+</div>
+</div>
+</div>
